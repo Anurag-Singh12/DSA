77a1d71a9b369cc40abe7c3e70de2389caca19f2
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2026-04-09 23:56:24 +0530

    L-4 Doubly Linked List

--- L-3 Singly Linked List/LL.py
+++ L-3 Singly Linked List/LL.py
@@ -22,6 +22,33 @@ class SinglyLinkedlist:
         temp = Node(value)
         temp.next = self.head
         self.head = temp
+
+    def insertInMid(self, value, x):
+        temp = Node(value)
+        t1 = self.head
+
+        while (t1.next != None):
+            if (t1.data == x):
+                temp.next = t1.next
+                t1.next = temp
+                break 
+            else:
+                t1 = t1.next    
+
+    def deleteLL(self, value):
+        t1 = self.head
+        prev = t1
+        if (t1.data == value):
+            self.head = t1.next
+        while (t1.next != None):
+            if (t1.data == value):
+                prev.next = t1.next
+                break
+            else:
+                prev = t1
+                t1 = t1.next
+        if (t1.data == value):
+            prev.next = None
     
     def printLL(self):
         t1 = self.head
@@ -36,4 +63,6 @@ obj.insertAtEnd(10)
 obj.insertAtEnd(20)
 obj.insertAtEnd(30)
 obj.insertAtBeg(5)
+obj.insertInMid(40,20)
+obj.deleteLL(30)
 obj.printLL()
