218a4335ecfa6bc132626401e613dbcea0b6d43b
Anurag-Singh12 <anuragsingh13creator@gmail.com>
2026-04-12 18:14:24 +0530

    L-7 Circular Queue

---
+++ L-7 Circular Queue/CQ.py
@@ -0,0 +1,44 @@
+class CircularQueue:
+    def __init__(self, size):
+        self.size = size
+        self.items = [None] * size
+        self.front = self.rear = -1
+
+    def enqueue(self, value):
+        if ((self.rear + 1) % self.size == self.front):
+            print("Queue is Full")
+        elif self.front == -1:
+            self.front = self.rear = 0
+            self.items[self.rear] = value
+        else:
+            self.rear = (self.rear + 1) % self.size
+            self.items[self.rear] = value
+    
+    def dequeue(self):
+        if(self.front == -1):
+            print("queue is empty")
+        elif self.front == self.rear:
+            print(self.items[self.front])
+            self.front = self.rear = -1
+        else:
+            print(self.items[self.front])
+            self.front = (self.front + 1) % self.size
+
+cq = CircularQueue(5)
+cq.enqueue(10)
+cq.enqueue(20)
+cq.enqueue(30)
+cq.enqueue(40)
+cq.enqueue(50)        #queue is full
+
+cq.dequeue()          #10 is removed
+cq.enqueue(60)        #60 is inserted in empty space of 10
+# cq.enqueue(70)      #60 it will show queue is full
+
+cq.dequeue()          #20 is removed
+cq.dequeue()          #similarly
+cq.dequeue()
+cq.dequeue()
+cq.dequeue()
+
+cq.dequeue()          #queue is empty
